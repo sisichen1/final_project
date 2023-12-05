61048ca30f617f949639f50fd5b4f567f4294ba5
sisichen1 <143667586+sisichen1@users.noreply.github.com>
2023-12-04 19:45:42 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// // const navN = document.querySelector('.nav');
+// // const hamburgerN = document.querySelector('.hamburger');
+
+// // hamburgerN.addEventListener('click',() => {
+// //     navN.classList.add('nav--open');
+// // });
+// const navEl = document.querySelector('.nav');
+// const hamburgerEl = document.querySelector('.hamburger');
+// const navItemEls = document.querySelectorAll('.nav__item');
+
+// hamburgerEl.addEventListener('click', () => {
+//   navEl.classList.toggle('nav--open');
+// });
+function change(element){
+    element.classList.toggle("fullsize_horizontal");
+}
+
+function change_vertical(element){
+    element.classList.toggle("fullsize_vertical");
+}
